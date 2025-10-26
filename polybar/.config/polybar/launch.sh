8539b9436dfff63b2c11b8060a4c8588c128fd5c
cristianpb <18708179+cristianpb@users.noreply.github.com>
2025-10-26 07:47:12 +0100

    🎨 Load API key to env variables before loading polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,6 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+source ~/.dotfiles/APIs
 # Launch bar1 and bar2
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload top &
